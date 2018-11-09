18000114167d4fb8959954ef21bd6c5cb5723d82
knarficious <knarficious@free.fr>
2018-11-09 12:23:22 +0100

    interface

---
+++ src/Knarf/PlatformBundle/Repository/Interfaces/AdvertRepositoryInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Knarf\PlatformBundle\Repository\Interfaces;
+
+/**
+ *
+ * @author franck
+ */
+interface AdvertRepositoryInterface 
+{
+    /**
+     * @param $requestVal
+     * @return array of movies
+     */
+    public function getResultFilterCount($requestVal);
+    
+    public function getResultFilterPaginated($requestVal, $limit = 20, $offset = 0);
+    
+    public function getQueryResultFilter($requestVal);
+}
