7661109adc8ed01a25ae6427a3d0b424f32c8674
knarficious <knarficious@free.fr>
2018-11-09 12:22:55 +0100

    add slug attribute and methods

--- src/Knarf/PlatformBundle/Entity/Advert.php
+++ src/Knarf/PlatformBundle/Entity/Advert.php
@@ -3,11 +3,11 @@
 namespace Knarf\PlatformBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\ArrayCollection;
 use Vich\UploaderBundle\Mapping\Annotation as Vich;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\HttpFoundation\File\File;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
  * Advert
@@ -47,6 +47,12 @@ class Advert
      * @ORM\Column(name="title", type="string", length=255)
      */
     private $title;
+    
+    /**
+     * @Gedmo\Slug(fields={"title"})
+     * @ORM\Column(length=255, unique=true)
+     */
+    private $slug;
 
     /**
      * @var string
@@ -153,9 +159,12 @@ class Advert
     public function setTitle($title)
     {
         $this->title = $title;
-
+        
+        $this->setSlug($this->title);
+        
         return $this;
-    }
+    }    
+
 
     /**
      * Get title
@@ -386,4 +395,56 @@ class Advert
     private $updateat;
 
 
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     *
+     * @return Advert
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $this->slugify($slug);
+
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+    
+    public function slugify($text)
+    {
+    // replace non letter or digits by -
+    $text = preg_replace('#[^\\pL\d]+#u', '-', $text);
+
+    // trim
+    $text = trim($text, '-');
+
+    // transliterate
+    if (function_exists('iconv'))
+    {
+        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+    }
+
+    // lowercase
+    $text = strtolower($text);
+
+    // remove unwanted characters
+    $text = preg_replace('#[^-\w]+#', '', $text);
+
+    if (empty($text))
+    {
+        return 'n-a';
+    }
+
+    return $text;
+    }
 }
