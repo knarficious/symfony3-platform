c01b5f0dcc1574b1cc5ab73e84f50df1fcc5e836
knarficious <knarficious@free.fr>
2019-01-08 15:38:22 +0100

    création d'un objet "avatar" différent de l'objet "profile" pour séparer les contraintes de validation des formulaires

---
+++ src/Knarf/UserBundle/Entity/User/Avatar.php
@@ -0,0 +1,110 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Knarf\UserBundle\Entity\User;
+
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+
+/**
+ * Description of Avatar
+ *
+ * @author franck
+ * 
+ * @Vich\Uploadable 
+ */
+class Avatar 
+{
+    /**
+     * @var user UserInterface
+     */
+    private $user;
+    
+    /**
+     * NOTE: This is not a mapped field of entity metadata, just a simple property.
+     * 
+     * @Assert\File(
+     * 		maxSize="10M",
+     * 		mimeTypes={"image/png", "image/jpeg", "image/gif"})
+     * @Vich\UploadableField(mapping="upload_media", fileNameProperty="nomMedia", nullable=true)
+     * 
+     * @var File
+     */
+    private $mediaFile;
+
+    /**
+     * @var string
+     */
+    private $nomMedia;
+    
+    public function __construct(UserInterface $user)
+    {
+        $this->user = $user;
+    }
+    
+    public function getUser()
+    {
+        return $this->user;
+    }
+    
+    /**
+     * Set nomMedia
+     *
+     * @param string $nomMedia
+     *
+     * @return User
+     */
+    public function setNomMedia($nomMedia)
+    {
+        $this->nomMedia = $nomMedia;
+
+        return $this;
+    }
+
+    /**
+     * Get nomMedia
+     *
+     * @return string
+     */
+    public function getNomMedia()
+    {
+        return $this->nomMedia;
+    }
+    
+    /**
+     * Set mediaFile
+     * 
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile $media
+     */
+     public function setMediafile($media)
+    {
+	$this->mediaFile = $media;
+		 
+	if ($media instanceof UploadedFile)
+        {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->setUpdatedAt();		
+        }	
+     }
+	 
+    /**
+     * Get mediaFile
+     * 
+     * @return File
+     */
+    public function getMediaFile()
+    {
+        return $this->mediaFile;
+    } 
+
+    
+}
