43099d985080f0e6988c34b6ae308e9dbbc00452
knarficious <knarficious@free.fr>
2019-01-15 21:50:15 +0100

    remove phpunit.xml.dist

---
+++ src/Knarf/UserBundle/Form/Type/User/ImageType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Knarf\PlatformBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ImageType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('url', TextType::class)->add('alt', TextType::class);
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Knarf\PlatformBundle\Entity\Image'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'knarf_platformbundle_image';
+    }
+
+
+}
