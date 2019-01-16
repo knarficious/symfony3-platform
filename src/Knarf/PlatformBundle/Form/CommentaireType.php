92ec1c9753e986e0121dc4147426357ae6c215fc
knarficious <knarficious@free.fr>
2019-01-16 03:40:47 +0100

    add Bootstrap modal

--- src/Knarf/PlatformBundle/Form/CommentaireType.php
+++ src/Knarf/PlatformBundle/Form/CommentaireType.php
@@ -18,9 +18,10 @@ class CommentaireType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('contenu',           TextareaType::class)
-                ->add('Enregistrer',       SubmitType::class)
+                ->add('Enregistrer',            SubmitType::class)
                 ->add('Effacer',           ResetType::class)
-                ->add('Annuler',           ButtonType::class);
+            //  ->add('Annuler',         ButtonType::class)
+               ;
     }
     
     /**
