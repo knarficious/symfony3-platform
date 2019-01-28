c92c2cb372d9b248b41e7bf8e9931908ab6ae283
knarficious <knarficious@free.fr>
2019-01-28 21:25:41 +0100

    update avatar methods

--- src/Knarf/UserBundle/Controller/UpdateProfileController.php
+++ src/Knarf/UserBundle/Controller/UpdateProfileController.php
@@ -13,9 +13,10 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
 use Knarf\UserBundle\Form\Type\User\EditEmailType;
-use Knarf\UserBundle\Form\Type\User\EditAvatarType;
+use Knarf\UserBundle\Form\Type\AvatarType;
+use Knarf\UserBundle\Form\Type\EditAvatarType;
 use Knarf\UserBundle\Entity\User\Profile;
-use Knarf\UserBundle\Entity\User\Avatar;
+use Knarf\UserBundle\Entity\Avatar;
 
 /**
  * Description of UpdateProfileController
@@ -33,8 +34,9 @@ class UpdateProfileController extends Controller
      */
     public function editEmailAction(Request $request)
     {
-        $data = new Profile($this->getUser());
-        $form = $this->createForm(EditEmailType::class, $data);
+        
+        $profile = new Profile($this->getUser());
+        $form = $this->createForm(EditEmailType::class, $profile);
         
         if($this->getEmailProfileFormHandler()->handle($form, $request))
         {
@@ -49,24 +51,110 @@ class UpdateProfileController extends Controller
     
     /**
      * @param Request $request
-     * @Route("/modifier-photo", name="change_image")
+     * @Route("/ajout-avatar", name="add_avatar")
      * @Method({"GET", "POST"})
      * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
-    public function editImageAction(Request $request)
-    {
-        $data = new Avatar($this->getUser());
-        $form = $this->createForm(EditAvatarType::class, $data);
+    public function addAvatarAction(Request $request)
+    {   
+        $user = $this->getUser();
         
-        if($this->getAvatarProfileFormHandler()->handle($form, $request))
-        {
-            $this->addFlash('notice', 'Votre profil a bien été mis à jour.');
+        if($user->getAvatar() !== null){
+            
+            $avatar = $user->getAvatar();            
+        }
+        
+        else {
+            
+            $avatar = new Avatar($user); 
+        }
+        
+        $em = $this->getDoctrine()->getManager();             
+
+        $form = $this->createForm(AvatarType::class, $avatar);
+        
+        if($request->isMethod('POST') && $form->handleRequest($request)->isValid())
+        {            
+            $em->persist($avatar);
+            $user->setAvatar($avatar);
+            $em->flush();
+            
+            $this->addFlash('notice', 'Votre avatar a bien été mis à jour.');
             return $this->redirect($this->generateUrl('profile'));
         }
         
-        return $this->render('KnarfUserBundle:Security:edit_image.html.twig', [
+        return $this->render('KnarfUserBundle:Security:add_avatar.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+    
+    /**
+     * @Route("/modifier-avatar", name="edit_avatar")
+     * @Method({"GET", "POST"})
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function editAvatarAction(Request $request)
+    {
+        $user = $this->getUser();
+        
+        if($user->getAvatar() !== null){
+            
+            $avatar = $user->getAvatar();            
+        }
+        
+        $em = $this->getDoctrine()->getManager();             
+
+        $form = $this->createForm(EditAvatarType::class, $avatar);
+        
+        if($request->isMethod('POST') && $form->handleRequest($request)->isValid())
+        {            
+            $avatar->setDate(new \DateTime());
+            $user->setUpdatedAt(new \DateTime());
+            $user->setAvatar($avatar);
+            $em->flush();
+            
+            $this->addFlash('notice', 'Votre avatar a bien été mis à jour.');
+            return $this->redirect($this->generateUrl('profile'));
+        }
+        
+        return $this->render('KnarfUserBundle:Security:edit_avatar.html.twig', [
+            'formulaire' => $form->createView(),
+        ]);
+    }
+    
+    /**
+     * @param Request $request
+     * @Route("/delete-avatar", name="delete_avatar")
+     * @Method({"GET", "POST"})
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function deleteAvatarAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        
+        $user = $this->getUser();
+        
+        $avatar = $user->getAvatar();
+        
+        $form = $this->createFormBuilder()->getForm();
+        
+        if($request->isMethod('POST') && $form->handleRequest($request)->isValid())
+            {
+                $em->remove($avatar);
+                $user->setAvatar(null);
+                $em->flush();
+        
+                $this->addFlash('notice', "L'avatar a été supprimé avec succès");
+        
+            return $this->redirect($this->generateUrl('profile'));    
+            }
+
+            return $this->render('KnarfUserBundle:Security:delete_avatar.html.twig', array(
+            'avatar'    => $avatar,
+            'form'      => $form->createView()
+            ));
+        
     }
     
     
