f812c1b21c724282e85fec73a6da14e2b99e6c0e
knarficious <knarficious@free.fr>
2019-09-06 16:48:09 +0200

    new MapController()

---
+++ src/Knarf/PlatformBundle/Controller/MapController.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Knarf\PlatformBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Description of MapController
+ *
+ * @author franck
+ */
+class MapController extends Controller
+{
+    public function mapViewAction()
+    {
+        
+    }
+}
