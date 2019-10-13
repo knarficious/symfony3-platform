815d98026d43d81c6be9974bb8d201e6477b9588
knarficious <knarficious@free.fr>
2019-10-14 00:41:28 +0200

    require vichfiletype

--- src/Knarf/PlatformBundle/Form/AdvertEditType.php
+++ src/Knarf/PlatformBundle/Form/AdvertEditType.php
@@ -4,6 +4,7 @@ namespace Knarf\PlatformBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Vich\UploaderBundle\Form\Type\VichFileType;
 
 class AdvertEditType extends AbstractType
 {
@@ -12,6 +13,11 @@ class AdvertEditType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         parent::buildForm($builder, $options);
+        $builder->add('mediaFile',  VichFileType::class, array(
+                                        'required' => false,
+                                        'label' => true,
+                                        'allow_delete' => true,
+                                    'download_uri' => false));
     }
     
     /**
