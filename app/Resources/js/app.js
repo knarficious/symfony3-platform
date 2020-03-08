03dc1b4b8ecd4715f67efcf22f747d115d80101e
knarficious <knarficious@free.fr>
2020-03-08 17:35:42 +0100

    add weather forecast widget

--- app/Resources/js/app.js
+++ app/Resources/js/app.js
@@ -5,5 +5,41 @@
  */
 require('../css/app.css');
 
+$(document).ready(function(){
+    
+  function weather() {
+  var location = document.getElementById("location");
+  var apiKey = "da6ae541c95e65f571c4274c3588cb5f";
+  var url = "https://api.darksky.net/forecast/";
+
+  navigator.geolocation.getCurrentPosition(success, error);
+
+  function success(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+
+    location.innerHTML =
+      "Latitude is " + latitude + "° Longitude is " + longitude + "°";
+
+    $.getJSON(
+      url + apiKey + "/" + latitude + "," + longitude + "?callback=?",
+      function(data) {
+        $("#temp").html(data.currently.temperature + "° F");
+        $("#minutely").html(data.minutely.summary);
+      }
+    );
+  }
+
+  function error() {
+    location.innerHTML = "Unable to retrieve your location";
+  }
+
+  location.innerHTML = "Locating...";
+}
+
+weather();
+});
+
+
 
 
