04ea8698d6fa1338ca98c86145fd3c429ef1c2f2
knarficious <knarficious@free.fr>
2020-03-08 23:16:12 +0100

    config weather forecast widget

--- app/Resources/js/app.js
+++ app/Resources/js/app.js
@@ -5,62 +5,46 @@
  */
 require('../css/app.css');
 
-$(document).ready(function(){
-    
-  function weather() {
-  var coordonnees = document.getElementById("coordonnees");
-  var location = document.getElementById("location");
-  var apiKey = "da6ae541c95e65f571c4274c3588cb5f";
-  var url = "https://api.darksky.net/forecast/";
+$(document).ready(function () {
 
-  navigator.geolocation.getCurrentPosition(success, error);
+    function weather() {
+        var process = document.getElementById("process");
+        var location = document.getElementById("location");
+        var apiKey = "da6ae541c95e65f571c4274c3588cb5f";
+        var url = "https://api.darksky.net/forecast/";
 
-  function success(position) {
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
-    
-    $.getJSON(
-            "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latitude + "," + longitude + "&key=AIzaSyBXnjaWQ1FinxrqxbwT34v90O0qxm2S9ZI",
-            function(data) {
-                location.innerHTML = data.plus_code.compound_code;     
-            }
-    ); 
-//    $.each(geocode, function(key, val) {
-//    location.html(key+ " *** " + val);
-//    });
-//    var geocode_length = geocode.length;
-//    for (var i = 0; i < geocode_length; i++)
-//    {
-//        location.html(geocode[i]["plus_code"]["compound_code"]);
-//    }
-    coordonnees.innerHTML =
-      "Latitude is " + latitude + "° Longitude is " + longitude + "°";
+        navigator.geolocation.getCurrentPosition(success, error);
 
-    $.getJSON(
-      url + apiKey + "/" + latitude + "," + longitude + "?lang=fr&units=auto&callback=?",
-      function(data) {
-//          var fahrenheit = data.currently.temperature;
-//          var celsius;
-//          if(fahrenheit !== '')
-//          {
-//              celsius = (fahrenheit - 32) * 5/9;
-//          }
-        $("#temp").html("Température extérieure: " + data.currently.temperature + "° C");
-        $("#icon").html(data.currently.icon);
-        $("#minutely").html(data.minutely.summary);
-      }
-    );
-  }
+        function success(position) {
+            latitude = position.coords.latitude;
+            longitude = position.coords.longitude;
 
-  function error(err) {
-    coordonnees.innerHTML = "Unable to retrieve your location";
-    console.warn(`ERROR(${err.code}): ${err.message}`);
-  }
+            $.getJSON(
+                    "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latitude + "," + longitude + "&key=AIzaSyBXnjaWQ1FinxrqxbwT34v90O0qxm2S9ZI",
+                    function (data) {
+                        location.innerHTML = data.plus_code.compound_code;
+                    }
+            );
 
-  coordonnees.innerHTML = "Locating...";
-}
+            $.getJSON(
+                    url + apiKey + "/" + latitude + "," + longitude + "?lang=fr&units=auto&callback=?",
+                    function (data) {
+                        $("#temp").html("Température extérieure: " + data.currently.temperature + "° C");
+                        $("#icon").html(data.currently.icon);
+                        $("#minutely").html(data.minutely.summary);
+                    }
+            );
+        }
 
-weather();
+        function error(err) {
+            process.innerHTML = "Unable to retrieve your location";
+            console.warn(`ERROR(${err.code}): ${err.message}`);
+        }
+
+        //process.innerHTML = "Locating...";
+    }
+
+    weather();
 });
 
 
