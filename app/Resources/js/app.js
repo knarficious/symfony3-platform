0367031f1a196c99427d40a0d91d65fe0a459eba
knarficious <knarficious@free.fr>
2020-03-12 17:59:10 +0100

    revert last commit

--- app/Resources/js/app.js
+++ app/Resources/js/app.js
@@ -12,60 +12,39 @@ $(document).ready(function () {
         var location = document.getElementById("location");
         var apiKey = "da6ae541c95e65f571c4274c3588cb5f";
         var url = "https://api.darksky.net/forecast/";
-        var latitude;
-        var longitude;
-        var networkInformation = navigator.connection.type;
-        if (networkInformation === 'ethernet')
-        {
-     
-            $.getJSON("https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyBXnjaWQ1FinxrqxbwT34v90O0qxm2S9ZI",
-                function(data)
-                {
-                   latitude = data.location.lat;
-                   longitude = data.location.lng;
-                });
-        }
-        else
-        {
-            navigator.geolocation.getCurrentPosition(success, error);
-            
-            function success(position) {
+
+        navigator.geolocation.getCurrentPosition(success, error);
+
+        function success(position) {
             latitude = position.coords.latitude;
             longitude = position.coords.longitude;
-            }
+
+            $.getJSON(
+                    "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latitude + "," + longitude + "&key=AIzaSyBXnjaWQ1FinxrqxbwT34v90O0qxm2S9ZI",
+                    function (data) {
+                        location.innerHTML = data.plus_code.compound_code.substring(data.plus_code.compound_code.indexOf(" "));
+                    }
+            );
+
+            $.getJSON(
+                    url + apiKey + "/" + latitude + "," + longitude + "?lang=fr&units=auto&callback=?",
+                    function (data) {
+                        $("#temp").html("Température extérieure: " + data.currently.temperature + "° C");
+                        $("#currently").html(data.currently.summary);
+                        $("#minutely").html(data.minutely.summary);
+                    }
+            );
         }
-        
+
         function error(err) {
             process.innerHTML = "Impossible de récupérer votre position";
             console.warn(`ERROR(${err.code}): ${err.message}`);
         }
-        
-        $.getJSON(
-                "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latitude + "," + longitude + "&key=AIzaSyBXnjaWQ1FinxrqxbwT34v90O0qxm2S9ZI",
-                function (data)
-                {
-                    location.innerHTML = data.plus_code.compound_code.substring(data.plus_code.compound_code.indexOf(" "));
-                }
-        );
-
-        $.getJSON(
-                url + apiKey + "/" + latitude + "," + longitude + "?lang=fr&units=auto&callback=?",
-                function (data) {
-                    $("#temp").html("Température extérieure: " + data.currently.temperature + "° C");
-                    $("#currently").html(data.currently.summary);
-                    $("#minutely").html(data.minutely.summary);
-                }
-        );
-
-        
-
-
 
         //process.innerHTML = "Locating...";
     }
 
-    weather();    
-
+    weather();
 });
 
 
