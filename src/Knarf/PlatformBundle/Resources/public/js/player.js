7c5a8831fa9dff69ea03b06124f3d26e63026fba
knarficious <knarficious@free.fr>
2020-05-04 17:10:51 +0200

    modify audio volume

--- src/Knarf/PlatformBundle/Resources/public/js/player.js
+++ src/Knarf/PlatformBundle/Resources/public/js/player.js
@@ -18,7 +18,6 @@ function init() {
     playlist = $('#playlist');
     tracks = playlist.find('li a');
     len = tracks.length - 1;
-    audio[0].volume = .10;
     audio[0].play();
     playlist.on('click','a', function (e) {
         e.preventDefault();
@@ -28,7 +27,7 @@ function init() {
     })
     audio[0].addEventListener('ended', function() {
         current++;
-        if (current === len) {
+        if (current > len) {
             current = 0;
             link = playlist.on('a')[0];
         } else {
