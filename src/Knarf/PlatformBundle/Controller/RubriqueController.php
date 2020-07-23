c12967b35294a0aa89da3790327b4c3b13d8c6ea
knarficious <knarficious@free.fr>
2020-07-24 01:41:18 +0200

    add meta properties for seo, og, twitter

--- src/Knarf/PlatformBundle/Controller/RubriqueController.php
+++ src/Knarf/PlatformBundle/Controller/RubriqueController.php
@@ -12,6 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Knarf\PlatformBundle\Form\RubriqueType;
 use Knarf\PlatformBundle\Form\RubriqueEditType;
 use Knarf\PlatformBundle\Entity\Rubrique;
@@ -22,20 +23,18 @@ use Knarf\PlatformBundle\Entity\Advert;
  *
  * @author franck
  */
-class RubriqueController extends Controller
-{
-    
+class RubriqueController extends Controller {
+
     /**
      * @Route("/", name="knarf_platform_home")
      * @return type
-     */    
-    public function indexAction()
-    {
+     */
+    public function indexAction() {
         $repository = $this->getDoctrine()->getManager()->getRepository('KnarfPlatformBundle:Rubrique');
         $listRubriques = $repository->getRubriquesEtAdverts();
-        
+
         return $this->render('KnarfPlatformBundle:Rubrique:index.html.twig', array(
-        'listRubriques' => $listRubriques
+                    'listRubriques' => $listRubriques
         ));
     }
 
@@ -43,25 +42,22 @@ class RubriqueController extends Controller
      * @Route("/rubriques", name="rubrique_ext_index")
      * @return type
      */
-    public function extendedIndexAction()
-    {
+    public function extendedIndexAction() {
         $repository = $this->getDoctrine()->getManager()->getRepository('KnarfPlatformBundle:Rubrique');
         $listRubriques = $repository->getRubriques();
-        
+
         return $this->render('KnarfPlatformBundle:Rubrique:index_ext.html.twig', array(
-        'listRubriques' => $listRubriques
+                    'listRubriques' => $listRubriques
         ));
     }
 
-        
     /**
      * @Route("/rubrique/{slug}", name="rubrique_ext")
      * @param type $slug
      * @return type
      * @throws NotFoundHttpException
      */
-    public function extendedViewAction($slug, Request $request)
-    {
+    public function extendedViewAction($slug, Request $request) {
         $em = $this->getDoctrine()->getManager();
         $rubrique = $em->getRepository('KnarfPlatformBundle:Rubrique')->findOneBy(['slug' => $slug]);
         $rubriqueId = $rubrique->getId();
@@ -72,38 +68,53 @@ class RubriqueController extends Controller
                 ->andWhere('a.isAdmin = 0')
                 ->setParameter('rubrique', $rubriqueId)
                 ->getQuery();
-        $paginator  = $this->get('knp_paginator');
+        $paginator = $this->get('knp_paginator');
         $pagination = $paginator->paginate(
-            $allAdvertsQuery,
-            $request->query->getInt('page', 1),
-            $request->query->getInt('limit', 8),
-            [ 'defaultSortFieldName' => 'a.date', 'defaultSortDirection' => 'desc' ]
-            );
-		 
-	return $this->render('KnarfPlatformBundle:Rubrique:ext.html.twig', array( 'rubrique' => $rubrique, 'pagination' => $pagination));		    
-    }    
+                $allAdvertsQuery,
+                $request->query->getInt('page', 1),
+                $request->query->getInt('limit', 8),
+                ['defaultSortFieldName' => 'a.date', 'defaultSortDirection' => 'desc']
+        );
+        $seoPage = $this->container->get('sonata.seo.page');
+        
+        $seoPage->addTitle($rubrique->getIntitule())
+                ->addMeta('name', 'description', $rubrique->getIntitule())
+                ->addMeta('property', 'og:locale', 'fr_FR')
+                ->addMeta('property', 'og:title', $rubrique->getIntitule())
+                ->addMeta('property', 'og:type', 'blog')
+                ->addMeta('property', 'og:url',  $this->generateUrl('rubrique_ext', ['slug'=> $rubrique->getSlug()
+                ], UrlGeneratorInterface::ABSOLUTE_URL))
+                ->addMeta('property', 'og:description', $rubrique->getIntitule())
+//                ->addMeta('property', 'og:image', $this->getParameter('base_url').$this->get('vich_uploader.templating.helper.uploader_helper')->asset($rubrique->getImage(), 'mediaFile'))
+                ->addMeta('property', 'article:published_time', date_format($rubrique->getUpdateAt(), 'd/m/Y'))
+                ->addMeta('property', 'twitter:card', 'summary')
+                ->addMeta('property', 'twitter:title', $rubrique->getIntitule())
+                ->addMeta('property', 'twitter:description', $rubrique->getIntitule())
+//                ->addMeta('property', 'twitter:image', $this->getParameter('base_url').$this->get('vich_uploader.templating.helper.uploader_helper')->asset($rubrique->getImage(), 'mediaFile'))
+                ->addMeta('property', 'twitter:url', $this->generateUrl('rubrique_ext', ['slug'=> $rubrique->getSlug()
+                ], UrlGeneratorInterface::ABSOLUTE_URL))
+                ;
 
-    public function viewAction($slug)
-    {        
+        return $this->render('KnarfPlatformBundle:Rubrique:ext.html.twig', array('rubrique' => $rubrique, 'pagination' => $pagination));
+    }
+
+    public function viewAction($slug) {
         $repository = $this
-        ->getDoctrine()
-        ->getManager()
-        ->getRepository('KnarfPlatformBundle:Rubrique');
+                ->getDoctrine()
+                ->getManager()
+                ->getRepository('KnarfPlatformBundle:Rubrique');
 
         $rubrique = $repository->findOneBy(array('slug' => $slug));
-        
-        if(null === $rubrique)
-        {
-            throw new NotFoundHttpException("La rubrique ".$slug." n'existe pas!");
-        }        
+
+        if (null === $rubrique) {
+            throw new NotFoundHttpException("La rubrique " . $slug . " n'existe pas!");
+        }
 
         return $this->render('KnarfPlatformBundle:Rubrique:view.html.twig', array(
-
-        'rubrique' => $rubrique
-
+                    'rubrique' => $rubrique
         ));
     }
-    
+
     /**
      * @Route("/rubrique/edit/{slug}", name="rubrique_edit")
      * @param type $slug
@@ -111,40 +122,35 @@ class RubriqueController extends Controller
      * @return type
      * @throws NotFoundHttpException
      */
-    public function editAction($slug, Request $request)
-    {
-
+    public function editAction($slug, Request $request) {
         $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'Unable to access this page!');
-        
+
         $em = $this->getDoctrine()->getManager();
-    
+
         $rubrique = $em->getRepository('KnarfPlatformBundle:Rubrique')->findOneBy(array('slug' => $slug));
-    
-        if(null === $rubrique)
-        {
-            throw new NotFoundHttpException("La rubrique ".$slug." n'existe pas!");
+
+        if (null === $rubrique) {
+            throw new NotFoundHttpException("La rubrique " . $slug . " n'existe pas!");
         }
-    
+
         $form = $this->createForm(RubriqueEditType::class, $rubrique);
-    
-        if($request->isMethod('POST') && $form->handleRequest($request)->isValid())
-        {
+
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
             $rubrique->setUpDateAt(new \DateTime());
             $em->persist($rubrique);
             $em->flush();
-        
+
             $request->getSession()->getFlashBag()->add('success', 'Rubrique modifiée avec succès.');
-        
+
             return $this->redirectToRoute('rubrique_ext', array('slug' => $rubrique->getSlug()));
         }
 
-
         return $this->render('KnarfPlatformBundle:Rubrique:edit.html.twig',
-                array(
-                    'rubrique' => $rubrique,
-                    'form' => $form->createView())
-                );
-
+                        [
+                            'rubrique' => $rubrique,
+                            'form' => $form->createView()
+                        ]
+        );
     }
 
     /**
@@ -154,66 +160,56 @@ class RubriqueController extends Controller
      * @return type
      * @throws NotFoundHttpException
      */
-    public function deleteAction($slug, Request $request)
-    {
+    public function deleteAction($slug, Request $request) {
 
         $em = $this->getDoctrine()->getManager();
-    
+
         $rubrique = $em->getRepository('KnarfPlatformBundle:Rubrique')->findOneBy(array('slug' => $slug));
-    
-        if(null === $rubrique)
-        {
-            throw new NotFoundHttpException("La rubrique ".$slug." n'existe pas!");
+
+        if (null === $rubrique) {
+            throw new NotFoundHttpException("La rubrique " . $slug . " n'existe pas!");
         }
-    
+
         $form = $this->createFormBuilder()->getForm();
-    
-        if($form->handleRequest($request)->isValid())
-        {
+
+        if ($form->handleRequest($request)->isValid()) {
             $em->remove($rubrique);
             $em->flush();
-        
-        $request->getSession()->getFlashBag()->add('success', "La rubrique a été supprimée avec succès");
-        
-        return $this->redirect($this->generateUrl('knarf_platform_home'));
-    
+
+            $request->getSession()->getFlashBag()->add('success', "La rubrique a été supprimée avec succès");
+
+            return $this->redirect($this->generateUrl('knarf_platform_home'));
         }
 
         return $this->render('KnarfPlatformBundle:Rubrique:delete.html.twig', array(
-        'rubrique'    => $rubrique,
-        'form'      => $form->createView()
+                    'rubrique' => $rubrique,
+                    'form' => $form->createView()
         ));
-
     }
-  
-    public function menuAction()
-    {
+
+    public function menuAction() {
 
         $repository = $this->getDoctrine()->getManager()->getRepository('KnarfPlatformBundle:Rubrique');
         $listRubriques = $repository->getRubriques();
 
 
         return $this->render('KnarfPlatformBundle:Rubrique:menu.html.twig', array(
- 
-
-        'listRubriques' => $listRubriques
-
+                    'listRubriques' => $listRubriques
         ));
     }
-    
+
     /**
      * @Route("/rubriques/ajout", name="rubrique_ajout")
      * @param Request $request
      * @return type
      */
-    public function addAction(Request $request)
-    {
+    public function addAction(Request $request) {
         $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'Unable to access this page!');
-        
+
         $rubrique = new Rubrique();
-        
+
         $rubrique->setUpdateAt(new \DateTime());
-        
+
         $form = $this->createForm(RubriqueType::class, $rubrique);
 
         if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
@@ -229,9 +225,8 @@ class RubriqueController extends Controller
         }
 
         // Si on n'est pas en POST, alors on affiche le formulaire
-    
-    return $this->render('KnarfPlatformBundle:Rubrique:ajout.html.twig', array('form' => $form->createView()));
-    
+
+        return $this->render('KnarfPlatformBundle:Rubrique:ajout.html.twig', array('form' => $form->createView()));
     }
-    
+
 }
